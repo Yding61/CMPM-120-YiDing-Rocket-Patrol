dee59d9d2682c5466ad730f990f8f1b8b8f029a4
YiDing <yding61@ucsc.edu>
2022-04-10 19:45:57 -0700

    RP 19.45 Rocket and Spaceship reset finished

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -43,13 +43,19 @@ class Play extends Phaser.Scene{
 
         // check clooision
         if (this.checkCollision(this.p1Rocket, this.ship01)) {
-            console.log('kaboom ship 01')
+            //console.log('kaboom ship 01')
+            this.p1Rocket.reset();
+            this.ship01.reset();
         }
         if (this.checkCollision(this.p1Rocket, this.ship02)) {
-            console.log('kaboom ship 02')
+            //console.log('kaboom ship 02')
+            this.p1Rocket.reset();
+            this.ship02.reset();
         }
         if (this.checkCollision(this.p1Rocket, this.ship03)) {
-            console.log('kaboom ship 03')
+            //console.log('kaboom ship 03')
+            this.p1Rocket.reset();
+            this.ship03.reset();
         }
     }
 
