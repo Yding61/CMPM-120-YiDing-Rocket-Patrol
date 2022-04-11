eb0c41c3c3b91e8b2e4674e2cde1efdd007dcbd2
YiDing <yding61@ucsc.edu>
2022-04-10 22:01:04 -0700

    RP 22.00 Audio finished

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,6 +11,9 @@ class Play extends Phaser.Scene{
         this.load.image('starfield','./assets/starfield.png');
         // load spritesheet
         this.load.spritesheet('explosion','./assets/explosion.png',{frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+
+
+
     }
     create() {
         // place tile sprite
@@ -137,6 +140,8 @@ class Play extends Phaser.Scene{
         // score add and repaint
         this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
+        // explosion audio
+        this.sound.play('sfx_explosion');
 
     }
 }
